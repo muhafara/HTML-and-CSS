d48cb82b055a029b051e840053882985e1c86559
muhafara <faranmuhammad@hotmail.com>
2021-10-25 16:16:43 +0100

    Basic_HTML_and_JavaScript_1
    Assignment 2.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hi, I'm linking to the external file.....!!")
+
+function My_First_Function(){
+var String ="Kiss me, I'm Irish";
+var result = String.fontcolor("green");
+document.getElementById("irish").innerHTML=result;
+}
